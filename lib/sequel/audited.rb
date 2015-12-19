c10d681261d40a38367c8804de8f8098eda1c5a1
Kematzy <kematzy@gmail.com>
2015-12-19 08:06:31 +0800

    Added global configuration options

--- lib/sequel/audited.rb
+++ lib/sequel/audited.rb
@@ -7,14 +7,22 @@ module Sequel
   module Audited
     
     # set the name of the global method that provides the current user. Default: :current_user
-    @audited_current_user_method = :current_user
-    # enable swapping of 
+    @audited_current_user_method      = :current_user
+    # enable swapping of the Audit model
     @audited_model_name               = :AuditLog
-    # toggle for 
-    @audited_enabled             = true
+    # toggle for enabling / disabling auditing
+    @audited_enabled                  = true
+    
+    # by default ignore these columns
+    @audited_default_ignored_columns  = [
+                                          # :id, :ref, :password, :password_hash, 
+                                          :lock_version, 
+                                          :created_at, :updated_at, :created_on, :updated_on
+                                        ]
     
     class << self
-      attr_accessor :audited_current_user_method, :audited_model_name, :audited_enabled
+      attr_accessor :audited_current_user_method, :audited_model_name,
+                    :audited_enabled, :audited_default_ignored_columns
     end
     
   end
--- spec/sequel/audited_spec.rb
+++ spec/sequel/audited_spec.rb
@@ -9,21 +9,75 @@ class Sequel::AuditedTest < Minitest::Test
       ::Sequel::Audited::VERSION.must_match %r{^\d+\.\d+\.\d+$}
     end
     
-    describe '#.audited_current_user_method' do
+    describe 'Class Methods' do
       
-      it 'can retrieve value' do
-        ::Sequel::Audited.audited_current_user_method.must_equal :current_user
-      end
-      
-      it 'can set & retrieve a value' do
-        # ::Sequel::Audited.methods.must_equal ''
-        ::Sequel::Audited.audited_current_user_method = :audited_user
-        ::Sequel::Audited.audited_current_user_method.must_equal :audited_user
-        ::Sequel::Audited.audited_current_user_method = :current_user  # reset
+      describe 'global configuration options' do
+        
+        describe '#.audited_current_user_method' do
+          
+          it 'should be :current_user' do
+            ::Sequel::Audited.audited_current_user_method.must_equal :current_user
+          end
+          
+          it 'can set & retrieve the new value' do
+            ::Sequel::Audited.audited_current_user_method = :audited_user
+            ::Sequel::Audited.audited_current_user_method.must_equal :audited_user
+            ::Sequel::Audited.audited_current_user_method = :current_user  # reset
+          end
+          
+        end
+        
+        describe '#.audited_model_name' do
+          
+          it 'should be :AuditLog' do
+            ::Sequel::Audited.audited_model_name.must_equal :AuditLog
+          end
+          
+          it 'can set & retrieve the new value' do
+            ::Sequel::Audited.audited_model_name = :DummyModel
+            ::Sequel::Audited.audited_model_name.must_equal :DummyModel
+            ::Sequel::Audited.audited_model_name = :AuditLog  # reset
+          end
+          
+        end
+        
+        describe '#.audited_enabled' do
+          
+          it 'should be: true' do
+            ::Sequel::Audited.audited_enabled.must_equal true
+          end
+          
+          it 'can set & retrieve the new value' do
+            ::Sequel::Audited.audited_enabled = false
+            ::Sequel::Audited.audited_enabled.must_equal false
+            ::Sequel::Audited.audited_enabled = true  # reset
+          end
+          
+        end
+        
+        describe '#.audited_default_ignored_columns' do
+          [
+            # :id, :ref, :password, :password_hash, 
+            :lock_version, :created_at, :updated_at, :created_on, :updated_on
+          ].each do |c|
+            it "should include: :#{c}" do
+              ::Sequel::Audited.audited_default_ignored_columns.must_include(c)
+            end
+          end
+          
+          it 'can set & retrieve the new value' do
+            old_val = ::Sequel::Audited.audited_default_ignored_columns
+            ::Sequel::Audited.audited_default_ignored_columns = [:dummy]
+            ::Sequel::Audited.audited_default_ignored_columns.must_equal [:dummy]
+            ::Sequel::Audited.audited_default_ignored_columns = old_val  # reset
+          end
+          
+        end
+        
       end
       
     end
-  
+    
   end
   
 end
